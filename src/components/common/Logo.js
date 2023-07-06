dc756a69fd5a47e9ae96de889cb469ec90120bb0
useongee <118250509+USEONGEE@users.noreply.github.com>
2023-07-06 20:20:53 +0900

    feat: Logo 컴포넌트 생성

---
+++ src/components/common/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+import logoImage from '../../assets/image/logo_instagram_text.png'
+
+const LogoContainer = styled.div`
+  height:${props => props.height};
+  width: ${props => props.width};
+`
+
+const LogoImg = styled.img` 
+  height:100%;
+  width:100%;
+`
+
+
+export default function Logo({height, width}) {
+  return (
+    <div>
+      <LogoContainer height={height} width={width}>
+        <LogoImg src={logoImage}/>
+      </LogoContainer>
+    </div>
+  )
+}
