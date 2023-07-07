2d3d30b37da50a33e19db62ca7779735491a3052
useongee <118250509+USEONGEE@users.noreply.github.com>
2023-07-07 21:09:22 +0900

    feat: router에 nav 추가

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
+import NavPage from '../pages/NavPage'
 
 export default function Router() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
+        <Route path='/MainPage' element={<NavPage />}>
+
+        </Route>
       </Routes>
     </div>
   )
