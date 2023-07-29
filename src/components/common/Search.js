5b3c13d2ac7edfb6bf26919c270c595521307028
premiree <rlaalfl05043@naver.com>
2023-07-30 02:33:39 +0900

    style:스타일 수정

--- src/components/common/Search.js
+++ src/components/common/Search.js
@@ -1,26 +1,27 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import styled from 'styled-components';
 import Modal from 'react-modal';
-import { Link } from 'react-router-dom';
+import { Link, useLocation,useNavigate } from 'react-router-dom';
 
 const ModalContainer = styled.div`
   // Modal을 구현하는데 전체적으로 필요한 CSS를 구현
-  width: 400px;
+  width: 350px;
   color: black;
   font-size: 16px;
   text-align: center;
-  padding: 20px;
+  padding: 10px;
   transition: transform 0.3s ease-in-out; 
   transform: ${props => props.isOpen ? 'translateX(0)' : 'translateX(100%)'}; 
 `;
 
+
 const SearchInput = styled.input`
-  width: 100%;
+  width: 350px;
   height: 25px;
   padding: 8px 16px;
-  background-color: #FAFAFA;
+  background-color: #f0f0f0;
   color: #000;
-  border: 1px solid #EFEFEF;
+  border: 1px solid #dbdbdb;
   border-radius: 5px;
   margin-bottom: 10px;
 `;
@@ -53,55 +54,78 @@ const SearchResultItem = styled.div`
   }
 `;
 const SearchTitle = styled.h1`
-  font-size: 30px;
+  font-size: 24px;
   text-align: left;
   margin-bottom: 10px;
 `;
 const RecentSearchTitle = styled.h2`
-
-  font-size: 16px;
+  font-size: 14px;
   text-align: left;
   margin-bottom: 10px;
 `;
-
 const ModalView = styled.div.attrs((props) => ({
   role: 'dialog',
 }))`
   align-items: center;
   flex-direction: column;
-  width: 400px;
+  width: 350px;
   padding: 20px;
   border-radius: 10px;
 `;
 
-const MagnifyingGlassButton = styled.button`
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
 
-  img {
-    width: 20px;
-    height: 20px;
-  }
-`;
 const ClearSearchLink = styled(Link)`
   font-size: 14px;
   text-align: left;
   color:blue;
   margin-top: 50px;
 `;
-const Search = ({ handleCloseModal }) => {
-  const [searchText, setSearchText] = useState('');
-  const [isOpen, setIsOpen] = useState(true);
-  const [recentSearches, setRecentSearches] = useState([]);
 
-  const searchResults = [
+const EmptySearchResult = styled.div`
+  font-size: 14px;
+  color: #888;
+  margin-top: 20px;
+`;
+
+const ConfirmModal = styled(Modal)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #fff;
+  padding: 20px;
+  width: 300px;
+  outline: none;
+  border-radius: 10px;
+`;
+
+const ConfirmButtons = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-top: 20px;
+  width:397px;
+`;
+
+const Button = styled.button`
+  padding: 10px;
+  background-color: #f0f0f0;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Search = ({ isModalOpen,handleCloseModal }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [searchText, setSearchText] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [confirmModalOpen, setConfirmModalOpen] = useState(false);
+  const [searchResults, setsearchResults] = useState([
+
     { id: 1, username: '사용자 1' },
     { id: 2, username: '사용자 2' },
     { id: 3, username: '사용자 3' },
-  ];
+  ]);
 
 
   const handleSearchInputChange = (e) => {
@@ -113,8 +137,20 @@ const Search = ({ handleCloseModal }) => {
   };
 
   const handleClearSearchHistory = () => {
-    setRecentSearches([]);
+   setsearchResults([]);
+   setConfirmModalOpen(false);
   };
+  const openConfirmModal = (e) => {
+    e.preventDefault();
+    setConfirmModalOpen(true);
+  };
+
+  const closeConfirmModal = () => {
+    setConfirmModalOpen(false);
+  };
+  const filteredResults = searchResults.filter((result) => 
+  result.username.toLowerCase().includes(searchText.toLowerCase())
+);
 
   const handleOpenModal = () => {
     setIsOpen(true);
@@ -122,19 +158,21 @@ const Search = ({ handleCloseModal }) => {
 
   const handleModalClose = () => {
     setIsOpen(false);
-    handleCloseModal();
-  };
-
-  const filteredResults = searchResults.filter((result) => 
-  result.username.toLowerCase().includes(searchText.toLowerCase())
-  );
+   navigate('/MainPage')  };
 
   return (
     <>
-      <MagnifyingGlassButton onClick={handleOpenModal}>
-        <img src="https://via.placeholder.com/20" alt="Magnifying Glass" />
-      </MagnifyingGlassButton>
-      <Modal isOpen={isOpen} onRequestClose={handleCloseModal}>
+      <Modal 
+      isOpen={isModalOpen} 
+      onRequestClose={handleCloseModal}
+      onClick={handleCloseModal}
+      style={{
+        content:{
+          width:'397px',
+          height:'100%',
+          margin:'20px',
+        }
+      }}>
         <ModalView onClick={handleModalClick}>
           <ModalContainer isOpen={isOpen}>
             <SearchTitle>검색</SearchTitle>
@@ -148,15 +186,22 @@ const Search = ({ handleCloseModal }) => {
               <RecentSearchTitle>최근 검색 항목</RecentSearchTitle>
               <ClearSearchLink to="/" onClick={handleClearSearchHistory}>모두 지우기 </ClearSearchLink>
             </ClearSearchContainer>
-            {filteredResults.map((result) => (
+            {filteredResults.length>0 ? filteredResults.map((result) => (
               <SearchResultItem key={result.id}>
                 <img src="https://via.placeholder.com/30" alt="profile" />
                 <span>{result.username}</span>
               </SearchResultItem>
-            ))}
+            )): <EmptySearchResult>최근 검색 내역 없음</EmptySearchResult>}
           </ModalContainer>
         </ModalView>
       </Modal>
+      <ConfirmModal isOpen={confirmModalOpen} onRequestClose={closeConfirmModal}>
+        <h2>검색 내역을 정말 지우시겠습니까?</h2>
+        <ConfirmButtons>
+          <Button onClick={handleClearSearchHistory}>예</Button>
+          <Button onClick={closeConfirmModal}>아니오</Button>
+        </ConfirmButtons>
+      </ConfirmModal>
     </>
   );
 }
