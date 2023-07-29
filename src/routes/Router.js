5e12dbdabdf14ff7063f0f5674a3c1470bc614ae
premiree <rlaalfl05043@naver.com>
2023-07-30 03:03:56 +0900

    refactor:/Explore 라우트 추가

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,17 +3,20 @@ import { Route, Routes } from 'react-router-dom'
 import LoginPage from '../pages/LoginPage'
 import NavPage from '../pages/NavPage'
 import HomePage from '../pages/HomePage'
-import Explore from '../components/Explore/Explore'
+import ExplorePage from '../pages/ExplorePage'
+
 export default function Router() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<LoginPage/>}/>
-        <Route path='/MainPage/*' element={<NavPage />}>
-          <Route path="/Explore" element = {Explore}/>
+        <Route path='/MainPage' element={<NavPage />}>
           <Route index element={<HomePage />}/>
+          </Route>
+        <Route path='/ExplorePage' element={<NavPage />}>
+          <Route index element={<ExplorePage />}/>
         </Route>
-      </Routes>
+        </Routes>
     </div>
   )
 }
